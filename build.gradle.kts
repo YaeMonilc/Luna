ba93364d3e74770115a7eecc586455fc9265fbbe
YaeMonilc <monilc@outlook.com>
2026-01-18 16:35:55 +0800

    Build: 完善构建脚本

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm") version "2.3.0"
+    kotlin("plugin.serialization") version "2.3.0"
+    id("com.gradleup.shadow") version "9.3.0"
 }
 
 group = "io.github.yaemonilc.reze.plugin.luna"
@@ -10,13 +14,27 @@ repositories {
 }
 
 dependencies {
+    compileOnly("io.github.yaemonilc.reze:Core")
 
+    // Source: https://mvnrepository.com/artifact/ai.koog/koog-agents
+    implementation("ai.koog:koog-agents:0.6.0")
 
     testImplementation(kotlin("test"))
 }
 
 kotlin {
     jvmToolchain(21)
+}
+
+tasks.register<Copy>("runPlugin") {
+    dependsOn("shadowJar")
+
+    tasks.named<ShadowJar>("shadowJar").apply {
+        from(get().archiveFile.get())
+        into("/Reze/plugins")
+    }
+
+    finalizedBy(gradle.includedBuild("Reze").task(":Launcher:run"))
 }
 
 tasks.test {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,10 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 rootProject.name = "Luna"
+
+includeBuild("Reze") {
+    dependencySubstitution {
+        substitute(module("io.github.yaemonilc.reze:Core"))
+            .using(project(":Core"))
+    }
+}
